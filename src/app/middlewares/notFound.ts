0858022f08900d54151939183534fbeab95b794a
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-31 22:37:35 +0600

    not found middleware added and zod installed

---
+++ src/app/middlewares/notFound.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from "express";
+import httpStatus from "http-status";
+
+const notFound: RequestHandler = (req, res, next) => {
+  return res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API Not Found",
+    error: "",
+  });
+};
