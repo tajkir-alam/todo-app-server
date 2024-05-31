ddca8d00d7826a1873c6e3a0632cd7b550af1ee8
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-05-31 22:55:21 +0600

    send response utils created

---
+++ src/app/routes/index.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const router = Router();
+
+const moduleRoutes = [
+  {
+    path: "/tasks",
+    route: "",
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
