f54a639ea704ceabffe2d4babbe297cff33acea7
MD Tajkir Alam Rion <tajkir.alam@gmail.com>
2024-06-01 14:08:59 +0600

    next function added on globallErrorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,9 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 const globalErrorHandler = (
   error: any,
   req: Request,
   res: Response,
+  next: NextFunction
 ) => {
   const status = 500;
   const message = "something went wrong";
